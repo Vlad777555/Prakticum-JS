59721556ba89c024d6794ed302d8f8ee71ab7f9e
vlad <reprintsev.vs@yandex.ru>
2020-09-30 13:52:11 +0300

    new app v2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
+let startBtn = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue = document.getElementsByClassName('monthsaivings-value')[0],
+    yearSavingsValue = document.getElementsByClassName('yearsaivings-value')[0],
+    data = document.getElementsByClassName('data'),
+
+    inputExpensesItem = document.getElementsByClassName('expenses-item'),
+    ExpensesBtn = document.getElementsByTagName('button')[0],
+    optionalExpensesBtn = document.getElementsByTagName('button')[1],
+    countBtn = document.getElementsByTagName('button')[2],
+    optionalExpensesItemInput = document.querySelectorAll('.optionalexpenses-item'),
+    incomeItem = document.querySelector('.choose-income'),
+	checkSavings = document.querySelector('#savings'),
+	sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+    
 let money, time;
 
 function start() {
